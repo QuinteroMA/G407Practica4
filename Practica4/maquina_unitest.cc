b1a0ab4bb684e9404b05b46ab582b075414a5322
lopbelfer95 <95644896+lopbelfer95@users.noreply.github.com>
2021-12-20 19:06:06 +0100

    Add files via upload

---
+++ Practica4/maquina_unitest.cc
@@ -0,0 +1,25 @@
+
+#include "maquina.h"
+#include "usuario.h"
+#include "gtest/gtest.h"
+
+TEST(maquina, comprobarMaquina) {
+maquina m(1,1);
+maquina m2(2,3);
+maquina m3(7,4);
+
+EXPECT_TRUE(m3.comprobarReserva());
+EXPECT_FALSE(m2.comprobarReserva());
+EXPECT_FALSE(m.comprobarReserva());
+
+}
+
+TEST(usuario, identificarse) {
+	usuario u1();
+ 	usuario u2();
+ 	usuario aux2("fernando","1234","fer@gmail.com","fernandoLopez",3,2,123456789,1);
+ 	usuario aux("","","","",0,0,0,0);
+ 	
+ 	EXPECT_EQ(aux, u1.identificarse("fallo","9999"));
+ 	EXPECT_EQ(aux2, u2.identificarse("fernando","1234"));
+}
